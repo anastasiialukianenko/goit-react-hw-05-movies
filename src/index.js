b0f2b0b48a92925bf08db26ff59881a15c520572
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-08-11 16:18:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'><App /></BrowserRouter>
+    <BrowserRouter basename='/goit-react-hw-05-movies/'><App /></BrowserRouter>
   </React.StrictMode>
 );
